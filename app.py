ef5106870587c550ec3f309b5fe7ee35e905b0af
Dinakarsaireddy <dhina3385@gmail.com>
2026-05-17 11:41:07 +0530

    Added drug form and database connection

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from config import db
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Polymer Drug App Running"
+    return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def add_drug():
+    drug= request.form['drug']
+    polymer=request.form['polymer']
+    solubility=request.form['solubility']
+    release=request.form['release']
+    circulation=request.form['circulation']
+
+    cursor = db.cursor()
+
+    query="""
+    INSERT INTO drugs
+    (drug_name, polymer_name, solubility, release_time, circulation_time)
+    VALUES (%s,%s,%s,%s,%s)
+    """
+
+    values= (drug,polymer, solubility,release,circulation)
+    cursor.execute(query,values)
+    db.commit()
+
+    return "Drug data saved successfully"
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
